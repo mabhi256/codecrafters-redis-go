3bdc76f2a2507bde47f5da62a8b4c7f47fded51a
Abhishek Mandingi <mabhi256@gmail.com>
2025-09-24 08:48:55 -0400

    #BW1 (Replication - Configure listening port)

--- app/main.go
+++ app/main.go
@@ -789,7 +789,14 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	var l net.Listener
+	var err error
+
+	if len(os.Args) == 3 && os.Args[1] == "--port" {
+		l, err = net.Listen("tcp", "0.0.0.0:"+os.Args[2])
+	} else {
+		l, err = net.Listen("tcp", "0.0.0.0:6379")
+	}
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
--- app/transaction.go
+++
@@ -1,4 +0,0 @@
-package main
-
-type TxnQueue struct {
-}
